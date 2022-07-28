1b14ed6a0877ce228377c045241981008342d374
CryptoTopStar <108870471+CryptoTopStar@users.noreply.github.com>
2022-07-28 15:05:45 -0700

    third

---
+++ src/services/user.service.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/test";
+const getPublicContent = () => {
+    return axios.get(API_URL + "all");
+};
+const getUserBoard = () => {
+    return axios.get(API_URL + "user", {headers: });
+};
